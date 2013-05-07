6f88f1e79bc1a068f68ff60356667ee8fcbf2be0
Aa`Koshh <aakoshh@gmail.com>
2013-05-07 17:36:38 +0100

    - solution for Tic-Tac-Toe-Tomek

---
+++ CodeJam/CodeJam/CodeJam2013QR.fs
@@ -0,0 +1,53 @@
+﻿namespace CodeJam
+open CodeJam.Utils
+open System
+
+
+// https://code.google.com/codejam/contest/2270488/dashboard
+module TicTacToeTomek = 
+    
+    type Result = Draw | Won of char | NotCompleted       
+
+    // enumerate all possible vectors
+    let candidates table = seq {
+        let maxi = (table |> Array2D.length1) - 1
+        let toVec arr = 
+            arr |> Array2D.toSeq |> Seq.map (fun (r,c,v) -> v) |> Seq.toArray
+        // rows and columns
+        for i in 0 .. maxi do
+            yield table.[i .. i, 0 .. maxi] |> toVec
+            yield table.[0 .. maxi, i .. i] |> toVec
+        // diagonals
+        yield [| for i in 0 .. maxi -> table.[i,i] |]
+        yield [| for i in 0 .. maxi -> table.[i,maxi-i] |]
+        }
+
+    let outcome vec = 
+        let cnt = vec |> Seq.countBy id |> Map.ofSeq
+        let wins c = 
+            match Map.tryFind c cnt with
+            | Some(4) -> true
+            | Some(3) -> Map.tryFind 'T' cnt = Some(1)
+            | _ -> false
+        if 'X' |> wins then Some(Won('X'))
+        elif 'O' |> wins then Some(Won('O'))
+        else None
+
+    let check table = 
+        let result = table 
+                    |> candidates 
+                    |> Seq.tryPick outcome
+        match result with
+        | Some(win) -> win
+        | _ -> if table |> Array2D.toSeq |> Seq.exists (fun (r,c,v) -> v = '.') then NotCompleted else Draw
+
+    let solve fn = 
+        // there is a case every 5 lines
+        solveFileBy (caseByN 5) fn <| fun s -> 
+            let table = (splitLines s).[.. 3] 
+                        |> Array.map (fun line -> [|for i in 0 .. 3 -> line.[i]|])
+                        |> array2D
+            table |> check |> (function 
+                | Won(side) -> sprintf "%c won" side
+                | Draw -> "Draw"
+                | NotCompleted -> "Game has not completed")
--- CodeJam/CodeJam/Script.fsx
+++ CodeJam/CodeJam/Script.fsx
@@ -2,15 +2,15 @@
 #load "CodeJamUtils.fs"
 # nowarn "25" // don't warn on incomplete pattern matches
 # nowarn "40"
-#load "CodeJam2011R1A.fs"
+#load "CodeJam2013QR.fs"
 
 fsi.AddPrinter (fun (x: bigint) -> sprintf "%AI" x)
 
 open CodeJam.Utils
 
 //CodeJam.SafetyInNumbers.solve "box-factory-small.in" |> printfn "%s"
-
-CodeJam.KillerWord.solve "killer-word-sample.in"
+CodeJam.TicTacToeTomek.solve "tictactoetomek-sample.in"
+CodeJam.TicTacToeTomek.solve "tictactoetomek-small-practice.in"
             
 
 
