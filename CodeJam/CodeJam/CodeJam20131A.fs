1f2fca81fb6e90e9dad486813c4f74898672fac3
Aa`Koshh <aakoshh@gmail.com>
2013-05-17 17:07:15 +0100

    - Bullseye small

---
+++ CodeJam/CodeJam/CodeJam20131A.fs
@@ -0,0 +1,23 @@
+﻿namespace CodeJam
+open CodeJam.Utils
+open System
+
+
+
+// https://code.google.com/codejam/contest/2418487/dashboard#s=p0
+module Bullseye = 
+
+    // paint needed to cover a ring starting at r
+    let paint (r: bigint) = 
+        ((r+1I) * (r+1I) - r * r) 
+
+    let count (r: bigint) (t: bigint) = 
+        let rec loop n t = 
+            let p = paint (r + 2I*n)
+            if t < p then n else loop (n+1I) (t-p)
+        loop 0I t
+
+    let solve fn = 
+        solveFile fn (fun line ->
+            let [|r;t|] = line |> splitSpaces |> Array.map (fun x -> bigint.Parse(x))
+            count r t |> string)
--- CodeJam/CodeJam/Script.fsx
+++ CodeJam/CodeJam/Script.fsx
@@ -2,14 +2,14 @@
 #load "CodeJamUtils.fs"
 # nowarn "25" // don't warn on incomplete pattern matches
 # nowarn "40"
-#load "CodeJam2013QR.fs"
+#load "CodeJam20131A.fs"
 
 fsi.AddPrinter (fun (x: bigint) -> sprintf "%AI" x)
 
 open CodeJam.Utils
          
-CodeJam.Treasure.solve "treasure-sample.in"
-CodeJam.Treasure.solve "treasure-small-practice.in"
+CodeJam.Bullseye.solve "treasure-sample.in"
+CodeJam.Bullseye.solve "treasure-small-practice.in"
 
 
 
